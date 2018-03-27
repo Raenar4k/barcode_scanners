6c0cb3ad05b84f5ffe59c59f732c05ea9244baa9
Ilia Voitcekhovskii <i.voytsekhovskiy@vi.local>
2018-03-27 12:05:46 +0300

    Добавил простую реализацию камеры и зависимости mobile vision

--- app/src/main/java/com/example/zxing/MainActivity.kt
+++ app/src/main/java/com/example/zxing/MainActivity.kt
@@ -1,25 +1,49 @@
 package com.example.zxing
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
+import io.fotoapparat.Fotoapparat
+import io.fotoapparat.configuration.CameraConfiguration
+import io.fotoapparat.log.logcat
+import io.fotoapparat.parameter.ScaleType
+import io.fotoapparat.selector.back
+import io.fotoapparat.view.CameraView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var button: Button
     private lateinit var textView: TextView
+    private lateinit var cameraView: CameraView
+    private lateinit var fotoapparat: Fotoapparat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button = findViewById(R.id.scan_button)
         textView = findViewById(R.id.result)
-        button.setOnClickListener { onButtonClick() }
+        cameraView = findViewById(R.id.camera_view)
+
+        fotoapparat = Fotoapparat(
+                context = this,
+                view = cameraView,                   // view which will draw the camera preview
+                scaleType = ScaleType.CenterCrop,    // (optional) we want the preview to fill the view
+                lensPosition = back(),               // (optional) we want back camera
+        cameraConfiguration = CameraConfiguration(
+                frameProcessor = {
+                }
+        ),
+                cameraErrorCallback = { error -> }   // (optional) log fatal errors
+        )
     }
 
-    private fun onButtonClick() {
+    override fun onStart() {
+        super.onStart()
+        fotoapparat.start()
+    }
 
+    override fun onStop() {
+        super.onStop()
+        fotoapparat.stop()
     }
 
 }
